a65168c86db1841bbfa37473b1c254e1a8d01645
KID <sqk_55@163.com>
2017-10-22 15:57:55 +0800

    add toTop

---
+++ js/toTop.js
@@ -0,0 +1,17 @@
+(function () {
+  var sdelay = null
+
+  function returnTop() {
+    window.scrollBy(0, -100)
+    if (document.body.scrollTop > 0) {
+      sdelay = requestAnimationFrame(returnTop)
+    }
+  }
+  window.addEventListener("mousewheel", function () {
+    cancelAnimationFrame(sdelay)
+  })
+  var toTop = document.getElementById("toTop")
+  toTop.addEventListener("click", function () {
+    returnTop()
+  })
+}());
