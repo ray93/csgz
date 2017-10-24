7ca2267287d63009bfa957654bbf83e13a466a0a
ray93 <xurui93@qq.com>
2017-10-24 21:26:49 +0800

    banner bgColor

--- js/index.js
+++ js/index.js
@@ -130,22 +130,26 @@
         banners: [{
             id: 1,
             url: '../img/index/banner/banner01.png',
+            bgColor: '#0a0b11'
           },
           {
             id: 2,
             url: '../img/index/banner/banner02.png',
+            bgColor: '#ffffff'
           },
           {
             id: 3,
             url: '../img/index/banner/banner03.png',
+            bgColor: '#fadc3e'
           }, {
             id: 4,
             url: '../img/index/banner/banner04.png',
+            bgColor: '#FBF8F5'
           }
         ],
         isOpen: false,
         brands: brands,
-        selectedType: "brands",
+        selectedType: "brands"
       }
     },
     methods: {
