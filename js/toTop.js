41b70902e73b16f0760f1dd8f0ed12b897991770
lfy55 <sqk_55@163.com>
2017-10-27 10:01:18 +0800

    fix toTop for mobile

--- js/toTop.js
+++ js/toTop.js
@@ -1,9 +1,21 @@
 (function () {
   var sdelay = null
 
+  function get_scrollTop_of_body() {
+    var scrollTop;
+    if (typeof window.pageYOffset != 'undefined') { //pageYOffset指的是滚动条顶部到网页顶部的距离
+      scrollTop = window.pageYOffset;
+    } else if (typeof document.compatMode != 'undefined' && document.compatMode != 'BackCompat') {
+      scrollTop = document.documentElement.scrollTop;
+    } else if (typeof document.body != 'undefined') {
+      scrollTop = document.body.scrollTop;
+    }
+    return scrollTop;
+  }
+
   function returnTop() {
     window.scrollBy(0, -100)
-    if (document.documentElement.scrollTop > 0) {
+    if (get_scrollTop_of_body() > 0) {
       sdelay = setTimeout(returnTop, 10)
     }
   }
