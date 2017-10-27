6459dc5f5124151fa06ce4cf9de2a452510fe5b7
lfy55 <sqk_55@163.com>
2017-10-27 09:47:53 +0800

    fix footer bug & toTop in moblie & add more news

--- js/toTop.js
+++ js/toTop.js
@@ -1,14 +1,26 @@
 (function () {
   var sdelay = null
 
+  function get_scrollTop_of_body() {
+    var scrollTop;
+    if (typeof window.pageYOffset != 'undefined') { //pageYOffset指的是滚动条顶部到网页顶部的距离
+      scrollTop = window.pageYOffset;
+    } else if (typeof document.compatMode != 'undefined' && document.compatMode != 'BackCompat') {
+      scrollTop = document.documentElement.scrollTop;
+    } else if (typeof document.body != 'undefined') {
+      scrollTop = document.body.scrollTop;
+    }
+    return scrollTop;
+  }
+
   function returnTop() {
     window.scrollBy(0, -100)
-    if (document.documentElement.scrollTop > 0) {
-      sdelay = requestAnimationFrame(returnTop)
+    if (get_scrollTop_of_body() > 0) {
+      sdelay = setTimeout(returnTop, 10)
     }
   }
   window.addEventListener("mousewheel", function () {
-    cancelAnimationFrame(sdelay)
+    clearTimeout(sdelay)
   })
   var toTop = document.getElementById("toTop")
   toTop.addEventListener("click", function () {
